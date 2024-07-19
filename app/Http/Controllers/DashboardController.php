760451c50c04a8d792a9fc4b645c6c9cc9d46199
harist13 <razak1352.com@gmail.com>
2024-07-20 00:08:09 +0800

    ajukan pengaduan

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Pengaduan;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -16,9 +18,32 @@ class DashboardController extends Controller
     {
         return view('masyarakat.components.pages.artikel');
     }
-    public function masyarakatpengaduan()
+     public function masyarakatpengaduan()
     {
-        return view('masyarakat.components.pages.pengaduan');
+        $pengaduan = Pengaduan::where('nik', Auth::guard('masyarakat')->user()->nik)->get();
+        return view('masyarakat.components.pages.pengaduan', compact('pengaduan'));
+    }
+
+    public function pengajuanPengaduan(Request $request)
+    {
+        $request->validate([
+            'tgl_pengaduan' => 'required|date',
+            'isi_laporan' => 'required|string',
+            'foto' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        $imageName = time().'.'.$request->foto->extension();  
+        $request->foto->move(public_path('images'), $imageName);
+
+        Pengaduan::create([
+            'tgl_pengaduan' => $request->tgl_pengaduan,
+            'nik' => Auth::guard('masyarakat')->user()->nik,
+            'isi_laporan' => $request->isi_laporan,
+            'foto' => $imageName,
+            'status' => '0', // 0 untuk status belum diproses
+        ]);
+
+        return redirect()->route('pengaduan')->with('success', 'Pengaduan berhasil diajukan');
     }
  
 
