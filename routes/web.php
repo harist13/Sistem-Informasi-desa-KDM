b5b90211b98d9346889f550fa453e1dde1bba9c3
Feek18 <bintangfikri31@gmail.com>
2024-07-20 21:41:57 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\TampilanAwalController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/login', function () {
+Route::get('', function () {
     return view('index');
 });
 
